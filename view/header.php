74490e79435d8aba649f3c98a70c6c8abaeae550
francs-lage <francs.lage@gmail.com>
2021-03-14 12:03:04 -0700

    finish creating all files and folders

---
+++ view/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zippy Used Autos </title>
+    <link rel="stylesheet" href="view/css/main.css">
+</head>
+<body>
+    <main>
+        <header><h1>Zippy Used Autos</h1></header>
