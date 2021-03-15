08a619561c1bd2e6c1bc051ad9c3fcf1a416ede7
Francs Lage <francs.lage@gmail.com>
2021-03-14 21:33:16 -0700

    Update database.php
    Heroku connection parameters

--- model/database.php
+++ model/database.php
@@ -1,6 +1,20 @@
 <?php
-    $dataSourceName = 'mysql:host=localhost;dbname=zippyusedautos';
-    $user = 'root';
+<?php
+    $dataSourceName = 'mysql:host=pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dbname=m9hrdx7mawh6mtur';
+    $user = 'jf60ce86hzd5yrp5';
+    $password = 'mpafrjoxiz9i7l7u';
+
+    try {
+        $database = new PDO($dataSourceName, $user, $password);
+    }
+    catch (PDOException $e) {
+        $error_message = 'Database Error: ';
+        $error_message .= $e->getMessage();
+        echo $error_message;
+        include('view/error.php');
+        exit();
+    }
+?>
 
     try {
         $database = new PDO($dataSourceName, $user);
